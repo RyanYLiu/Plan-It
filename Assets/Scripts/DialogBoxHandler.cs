cb3665d52cd83545e3d59578980006c65e242d78
Ryan <ryanliu967@gmail.com>
2020-10-12 16:15:02 -0700

    Add basic functions to move on map

---
+++ Assets/Scripts/DialogBoxHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogBoxHandler : MonoBehaviour
+{
+    // public GameObject popUpBox;
+    // public Animator popUpAnimator;
+    // public string popUpText;
+    PlayerMap player;
+    Scouter scouter;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMap>();
+        scouter = FindObjectOfType<Scouter>();
+    }
+
+    public void CloseDialogBox()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ConfirmScoutClickHandler()
+    {
+        scouter.SetMove(true);
+        CloseDialogBox();
+    }
+
+    public void ConfirmPlayerClickHandler()
+    {
+        player.SetMove(true);
+        CloseDialogBox();
+    }
+}
---
+++ Assets/Scripts/PlayerMap.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMap : MonoBehaviour
+{
+    private bool move = false;
+     private GameObject destination;
+     [SerializeField] float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move)
+        {
+            MoveToNode();
+        }
+    }
+
+    public void Focus(bool status)
+    {
+        if (status)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(255f,255f,255,1f);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = new Color(255f,255f,255,0.5f);
+        }
+    }
+
+    public void SetMove(bool val)
+    {
+        GetComponent<Animator>().enabled = !val;
+        move = val;
+    }
+
+    private void MoveToNode()
+    {
+        transform.position = Vector2.MoveTowards(
+                                transform.position,
+                                destination.transform.position,
+                                moveSpeed * Time.deltaTime);
+        if (transform.position == destination.transform.position)
+        {
+            SetMove(false);
+        }
+    }
+    
+    public void SetDestination(GameObject node)
+    {
+        destination = node;
+    }
+}
